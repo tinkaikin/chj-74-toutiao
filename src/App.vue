8024d50396c650257fe7e2ad5205f42afd20822d
tinkaikin <tinkaikin@163.com>
2019-07-09 17:50:55 +0800

    安装了 ElementUI

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     App
+    <el-row>
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
-
 }
 </script>
 
 <style lang="less">
-
 </style>
