ab23e120b0f013722b8071f065bd890b3d4e3240
tinkaikin <tinkaikin@163.com>
2019-07-09 20:26:15 +0800

    登录完成一部份

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="container">
-    login
+  <div class="login-container">
+    <el-card class="login-card">
+      <img src="../../assets/images/logo_index.png" alt="">
+    </el-card>
   </div>
 </template>
 
@@ -11,5 +13,25 @@ export default {
 </script>
 
 <style lang="less">
-
+.login-container{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url('../../assets/images/login_bg.jpg') no-repeat center;
+  background-size: 100% 100%;
+  .login-card{
+    width: 400px;
+    height: 300px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    img{
+      display:block;
+      margin:0 auto;
+    }
+  }
+}
 </style>
