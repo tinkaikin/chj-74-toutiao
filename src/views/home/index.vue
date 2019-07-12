48cf55f6c88e1acf39a1081f365c48e457e459ce
tinkaikin <tinkaikin@163.com>
2019-07-12 09:13:36 +0800

    侧边栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,46 @@
 <template>
   <el-container class="home-container">
-    <el-aside width="200px" class="home-aside">Aside</el-aside>
+    <el-aside width="200px" class="home-aside">
+      <div class="logImg"></div>
+
+      <el-menu
+        style="border-right:none"
+        default-active="1"
+        class="el-menu"
+        background-color="#002033"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span slot="title">内容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+
+    </el-aside>
     <el-container>
       <el-header class="home-header">
         <span class="el-icon-s-fold"></span>
@@ -34,6 +74,10 @@ export default {}
   left: 0;
   .home-aside {
     background-color: #002033;
+    .logImg{
+      height: 60px;
+      background: rgb(0,40,40) url('../../assets/images/logo_admin.png') no-repeat center;
+    }
   }
   .home-header {
     border-bottom: 1px solid #ddd;
