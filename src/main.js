50df0afffd986f0e08e771e983072af6036a51e8
tinkaikin <tinkaikin@163.com>
2019-07-12 22:11:52 +0800

    axios-请求拦截器

--- src/main.js
+++ src/main.js
@@ -7,7 +7,23 @@ import 'element-ui/lib/theme-chalk/index.css'
 import router from './routers'
 import axios from 'axios'
 
-Vue.prototype.$http = axios
+const instance = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/mp/v1_0/'
+})
+instance.interceptors.request.use(config => {
+  const user = window.sessionStorage.getItem('chj74-toutiao')
+  if (user) {
+    config.headers = {
+      Authorization: 'Bearer ' + JSON.parse(user).token
+    }
+  }
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+Vue.prototype.$http = instance
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
