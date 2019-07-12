27e8c5f86c41341653bd0cbe40b56bbbc3fc8d62
tinkaikin <tinkaikin@163.com>
2019-07-12 22:24:11 +0800

    axios-响应拦截器

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/mp/v1_0/'
 })
+// 请求拦截
 instance.interceptors.request.use(config => {
   const user = window.sessionStorage.getItem('chj74-toutiao')
   if (user) {
@@ -19,6 +20,15 @@ instance.interceptors.request.use(config => {
   }
   return config
 }, error => {
+  return Promise.reject(error)
+})
+// 响应拦截
+instance.interceptors.response.use(response => response, error => {
+  // 做一些事情
+  if (error.response.status === 401) {
+    // hash 哈希   是url后  #开始的字符串
+    location.hash = '#/login'
+  }
   return Promise.reject(error)
 })
 
