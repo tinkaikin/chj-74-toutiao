af6585bbb2ef6a0187b2c739cfc27315d87b93f6
tinkaikin <tinkaikin@163.com>
2019-07-13 09:53:23 +0800

    axios对象抽离封装

---
+++ src/api/axios.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/mp/v1_0/'
+})
+// 请求拦截
+instance.interceptors.request.use(config => {
+  const user = window.sessionStorage.getItem('chj74-toutiao')
+  if (user) {
+    config.headers = {
+      Authorization: 'Bearer ' + JSON.parse(user).token
+    }
+  }
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+// 响应拦截
+instance.interceptors.response.use(response => response, error => {
+  // 做一些事情
+  if (error.response.status === 401) {
+    // hash 哈希   是url后  #开始的字符串
+    location.hash = '#/login'
+  }
+  return Promise.reject(error)
+})
+
+export default instance
--- src/main.js
+++ src/main.js
@@ -5,33 +5,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import router from './routers'
-import axios from 'axios'
-
-const instance = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/mp/v1_0/'
-})
-// 请求拦截
-instance.interceptors.request.use(config => {
-  const user = window.sessionStorage.getItem('chj74-toutiao')
-  if (user) {
-    config.headers = {
-      Authorization: 'Bearer ' + JSON.parse(user).token
-    }
-  }
-  return config
-}, error => {
-  return Promise.reject(error)
-})
-// 响应拦截
-instance.interceptors.response.use(response => response, error => {
-  // 做一些事情
-  if (error.response.status === 401) {
-    // hash 哈希   是url后  #开始的字符串
-    location.hash = '#/login'
-  }
-  return Promise.reject(error)
-})
-
+// 导入axios实例
+import instance from './api/axios'
 Vue.prototype.$http = instance
 
 Vue.use(ElementUI)
